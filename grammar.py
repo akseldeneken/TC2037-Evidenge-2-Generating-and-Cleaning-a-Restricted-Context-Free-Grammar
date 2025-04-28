c71665db5f8b435114f4fab87751d61aae7866fe
akseldeneken <149641642+akseldeneken@users.noreply.github.com>
2025-04-28 13:32:55 -0600

    Update grammar.py

--- grammar.py
+++ grammar.py
@@ -53,7 +53,9 @@ def main():
             "eles come o filme",
             "bebe tu os futebol", # Incorrect
             "água bebe a tu",  # Incorrect
-            "eu filme vê as"  # Incorrect
+            "eu filme vê as",  # Incorrect
+            "es pão come a", # Incorrect
+            "elle ama futebol"   # Incorrect
         ]
 
     # Itirate over each predefined sentence and test it with the grammar "test_sentence(sent)"
